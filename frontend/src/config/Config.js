f4fa3d10dcdc5c616f4939774a227736f8251c2b
AmaanNaseh <naseh.amaan@gmail.com>
2025-12-29 00:06:18 +0530

    updated config.js

--- frontend/src/config/Config.js
+++ frontend/src/config/Config.js
@@ -1,1 +1,2 @@
-export const ml_backend_url = "http://localhost:8000";
+// export const ml_backend_url = "http://localhost:8000";
+export const ml_backend_url = "https://pulmoncare-backend.onrender.com";
